03b4e978bbf09ad304805c928d35153daa69f22a
Ashish Alexander <ashishalex29@gmail.com>
2025-05-03 01:53:34 +0530

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import { writeFileSync } from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'generate-spa-redirect',
-      closeBundle() {
-        // Create _redirects file for Render/Netlify
-        writeFileSync('./dist/_redirects', '/* /index.html 200');
-        
-        // Create a rewrite rule file for Render
-        writeFileSync('./dist/200.html', '<script>window.location.pathname = "/" + window.location.href.split("/").slice(3).join("/");</script>');
-      }
-    }
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src")
